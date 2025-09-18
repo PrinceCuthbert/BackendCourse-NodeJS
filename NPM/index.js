1c519616981e556fc7eba7a6fa737c68c53b398d
ISHIMWE Prince Cuthbert <ishimweprincecuthbert@gmail.com>
2025-09-18 23:38:50 +0300

    First codes of backend-NodeJS/Angela's course

---
+++ NPM/index.js
@@ -0,0 +1,15 @@
+import generateName from "sillyname";
+
+// import superheroes from "superheroes";
+
+import { randomSuperhero } from "superheroes";
+
+// var generateName = require("sillyname");
+
+// const sillyName = generateName();
+
+const name = randomSuperhero();
+
+// console.log(`My name is ${sillyName}`);
+
+console.log(`I'm ${name}`);
